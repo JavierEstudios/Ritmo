ee5e98cb912da4fa1b3738b87477ecb0486b7ba0
JavierEstudios <xjfaravi975@ieshnosmachado.org>
2023-10-10 10:03:27 +0200

    cargar el json

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 import json
 
+url = "https://github.com/JavierEstudios/ritmo/blob/3d78a3056ab875dbdb0d8d061cfad0bfe645c802/canciones.json"
+
+file = open(url)
+
+data = json.load(file)
+
+for i in data["canciones"]:
+    print (i)
+
+file.close()
