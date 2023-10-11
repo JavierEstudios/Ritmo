06a57ebfe8cb31b0d03f9c87a8ae747670267c32
JavierEstudios <xjfaravi975@ieshnosmachado.org>
2023-10-11 10:34:36 +0200

    Mostrar datos (solo pasado)

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 import json
 import requests
 
-headers = {'Accept': 'application/json'}
+#headers = {'Accept': 'application/json'}
+#url = ('https://github.com/JavierEstudios/ritmo/blob/ee5e98cb912da4fa1b3738b87477ecb0486b7ba0/canciones.json')
+#file = requests.get(url)
+#data = file.json()
 
-url = ("https://github.com/JavierEstudios/ritmo/blob/ee5e98cb912da4fa1b3738b87477ecb0486b7ba0/canciones.json")
+#print(data)
 
-file = requests.get(url, headers=headers)
+file = open("canciones.json")
+data = json.load(file)
 
-#url = ("canciones.json")
+cc = 1
+while cc < 7.5:
+    i = 0
+    for canciones in data['canciones']:
+        if data['canciones'][i]['pst_cc'] == cc:
+            print ("Nombre: ",data['canciones'][i]['nombre'],", pack: ",data['canciones'][i]['pack'],", dificultad: ",data['canciones'][i]['pst'],", lado: ",data['canciones'][i]['lado'],", duración: ",data['canciones'][i]['length'])
+        i = i+1
+    cc = cc+0.5
 
-#file = open(canciones.json)
+#for canciones in data:
+#    print(data['canciones'])
 
-data = file.json()
-
-for i in data:
-    print(i, ":", data[i])
-
-file.close()
+##file.close()
