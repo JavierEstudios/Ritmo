6069f023fd65500b2d956781f9516c3b5fb7d338
JavierEstudios <xjfaravi975@ieshnosmachado.org>
2023-10-11 09:10:54 +0200

    Haciendo pruebas

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import json
 import requests
 
+headers = {'Accept': 'application/json'}
+
 url = ("https://github.com/JavierEstudios/ritmo/blob/ee5e98cb912da4fa1b3738b87477ecb0486b7ba0/canciones.json")
 
-file = requests.get(url)
+file = requests.get(url, headers=headers)
+
+#url = ("canciones.json")
+
+#file = open(canciones.json)
 
 data = file.json()
 
-for i in data['canciones']:
-    print (i)
+for i in data:
+    print(i, ":", data[i])
 
 file.close()
