6520adf44b0ed8b90f96bb05681aebb56189444a
JavierEstudios <xjfaravi975@ieshnosmachado.com>
2023-10-16 20:13:19 +0200

    mostrar datos completo

--- main.py
+++ main.py
@@ -11,14 +11,46 @@ import requests
 file = open("canciones.json")
 data = json.load(file)
 
-cc = 1
-while cc < 7.5:
+## Valores predeterminados
+cc = 'prs_cc'
+mincc = 1
+maxcc = 12
+level = 'prs'
+
+selectDone = False
+dificultad = input("Introduzca la dificultad en la que quiere ordenar las canciones (pasado, presente, futuro o beyond): ")
+while selectDone == False:
+    selectDone = True
+    if dificultad == 'pasado':
+        cc = 'pst_cc'
+        maxcc = 7
+        level = 'pst'
+    elif dificultad == 'presente':
+        mincc = 3.5
+        maxcc = 9.5
+    elif dificultad == 'futuro':
+        cc = 'ftr_cc'
+        mincc = 7
+        maxcc = 11.3
+        level = 'ftr'
+    elif dificultad == 'beyond':
+        cc = 'byd_cc'
+        mincc = 9.5
+        level = 'byd'
+    else:
+        dificultad = input("Input no reconocido, vuelva a introducurlo: ")
+        selectDone = False
+
+
+while mincc <= maxcc:
     i = 0
     for canciones in data['canciones']:
-        if data['canciones'][i]['pst_cc'] == cc:
-            print ("Nombre: ",data['canciones'][i]['nombre'],", pack: ",data['canciones'][i]['pack'],", dificultad: ",data['canciones'][i]['pst'],", lado: ",data['canciones'][i]['lado'],", duración: ",data['canciones'][i]['length'])
+        if data['canciones'][i][cc] == mincc:
+            print ("Nombre: ",data['canciones'][i]['nombre'],", pack: ",data['canciones'][i]['pack'],", dificultad: ",data['canciones'][i][level],", bando: ",data['canciones'][i]['lado'],", duración: ",data['canciones'][i]['length'])
         i = i+1
-    cc = cc+0.5
+    if mincc < 8:
+        mincc = mincc+0.5
+    else: mincc = round(mincc+0.1, 1)
 
 #for canciones in data:
 #    print(data['canciones'])
