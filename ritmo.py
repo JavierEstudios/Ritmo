190ef2e6e35ca21ace20316a7596ff23834d8d0e
JavierEstudios <xjfaravi975@ieshnosmachado.com>
2023-10-20 16:08:29 +0200

    opcion para mostrar solo las canciones de un pack

--- ritmo.py
+++ ritmo.py
@@ -1,17 +1,43 @@
 import requests
 
-def funcionPrincipal(file):
+def funcionPrincipal():
     ## Valores predeterminados para que python no se queje
     cc = 'prs_cc'
     mincc = 1
     maxcc = 12
     level = 'prs'
+    ## Packs en el juego
+    listaPacks = ["Eternal Core","Crimson Solace","Arcaea","Memory Archive","Ambivalent Vision","Vicious Labyrinth","Binary Enfold","Luminous Sky","Absolute Reason","Adverse Prelude","Sunset Radiance","Extend Archive","Black Fate","Ephemeral Page","Esoteric Order","Divided Heart","Final Verdict","Silent Answer","World Extend","Lasting Eden"]
+
+    ## Preguntar si mostrar los packs del juego
+    print("El Programa muestra el listado de canciones de un pack del juego, ordenadas por dificultad.")
+    mostrarPacks = input("¿Quiere ver la lista de packs? (si o no): ")
+    if mostrarPacks == 'si':
+        for i in listaPacks:
+            print(i)
+
+    ## Seleccionar el pack del que se muestran las canciones
+    pack = input("Introduzca de que pack quiere que sean las canciones. Puede dejar el campo vacio para mostrar todas las canciones: ")
+    selectPack = False
+    packCorrecto = False
+    if pack == '':
+        print("Mostrando todas las canciones")
+        packCorrecto = True
+    while packCorrecto == False:
+        for i in listaPacks:
+            if i == pack: 
+                selectPack = True
+                packCorrecto = True
+                print("Mostrando las canciones de " + pack)
+        if packCorrecto == False:
+            print(pack + " no es un input valido")
+            pack = input("Introduzca de que pack quiere que sean las canciones: ")
 
     ## Seleccionar la dificultad en la que se recorren las canciones
-    selectDone = False
+    selectDiff = False
     dificultad = input("Introduzca la dificultad en la que quiere ordenar las canciones (pasado, presente, futuro o beyond): ")
-    while selectDone == False:
-        selectDone = True
+    while selectDiff == False:
+        selectDiff = True
         if dificultad == 'pasado':
             cc = 'pst_cc'
             maxcc = 7
@@ -30,10 +56,11 @@ def funcionPrincipal(file):
             level = 'byd'
         else:
             dificultad = input("Input no reconocido, vuelva a introducurlo: ")
-            selectDone = False
-    todasLasCanciones(file, cc, mincc, maxcc, level)
+            selectDiff = False
+    if selectPack: cancionesPorPack(cc, mincc, maxcc, level, pack)
+    else: todasLasCanciones(cc, mincc, maxcc, level)
 
-def todasLasCanciones(file, cc, mincc, maxcc, level):
+def todasLasCanciones(cc, mincc, maxcc, level):
     ## Recorrer las canciones
     while mincc <= maxcc:
         i = 0
@@ -46,12 +73,37 @@ def todasLasCanciones(file, cc, mincc, maxcc, level):
                 nivel = data['canciones'][i][level]
                 bando = data['canciones'][i]['lado']
                 duracion = data['canciones'][i]['length']
-                print ("Nombre: {0:43s} Pack: {1:22s} Colección: {2:20s} Dificultad: {3:3s} Bando: {4:9s} Duración: {5:7s}"
+                print ("Nombre: {0:43s} Pack: {1:17s} Colección: {2:20s} Dificultad: {3:3s} Bando: {4:9s} Duración: {5:7s}"
                     .format(cancion, pack, coleccion, nivel, bando, duracion))
-            i = i+1
-        if mincc < 8:
-            mincc = mincc+0.5
+            i += 1
+        if mincc < 8: mincc += 0.5
         else: mincc = round(mincc+0.1, 1)
+
+def cancionesPorPack(cc, mincc, maxcc, level, pack):
+    recorrer = True
+    if level == 'byd':
+        packsSindBYD = ["Sunset Radiance","Extend Archive","Ephemeral Page","Esoteric Order","Divided Heart","World Extend","Lasting Eden"]
+        for i in packsSindBYD:
+            if i == pack: 
+                print(pack + " no tiene ninguna canción con dificultad beyond")
+                recorrer = False
+    ## Recorrer las canciones
+    if recorrer:
+        while mincc <= maxcc:
+            i = 0
+            for canciones in data['canciones']:
+                if data['canciones'][i][cc] == mincc and data['canciones'][i]['pack'] == pack:
+                    cancion = data['canciones'][i]['nombre']
+                    coleccion = data['canciones'][i]['col']
+                    if coleccion == None: coleccion = 'Ninguna'
+                    nivel = data['canciones'][i][level]
+                    bando = data['canciones'][i]['lado']
+                    duracion = data['canciones'][i]['length']
+                    print ("Nombre: {0:43s} Colección: {1:20s} Dificultad: {2:3s} Bando: {3:9s} Duración: {4:7s}"
+                        .format(cancion, coleccion, nivel, bando, duracion))
+                i += 1
+            if mincc < 8: mincc += 0.5
+            else: mincc = round(mincc+0.1, 1)
 
 ## Cojer el json de internet
 try:
@@ -61,5 +113,5 @@ except:
 else:
     data = file.json()
     ## Llamar al resto del programa
-    funcionPrincipal(file)
+    funcionPrincipal()
     file.close()
